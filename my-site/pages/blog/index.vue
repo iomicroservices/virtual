d1e32f0731f28526ccb761ed95d27b9fcfc8fc41
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-02-28 04:21:21 +0000

    content for blog

--- my-site/pages/blog/index.vue
+++ my-site/pages/blog/index.vue
@@ -1,2 +1,135 @@
+<script lang="ts" setup>
+const { data } = await useAsyncData('home', () => queryContent('/blog').sort({ _id: -1 }).find())
+
+const elementPerPage = ref(5)
+const pageNumber = ref(1)
+const searchTest = ref('')
+
+const formattedData = computed(() => {
+  return data.value?.map((articles) => {
+    return {
+      path: articles._path,
+      title: articles.title || 'no-title available',
+      description: articles.description || 'no-description available',
+      image: articles.image || '/not-found.jpg',
+      alt: articles.alt || 'no alter data available',
+      ogImage: articles.ogImage || '/not-found.jpg',
+      date: articles.date || 'not-date-available',
+      tags: articles.tags || [],
+      published: articles.published || false,
+    }
+  }) || []
+})
+
+const searchData = computed(() => {
+  return formattedData.value.filter((data) => {
+    const lowerTitle = data.title.toLocaleLowerCase()
+    if (lowerTitle.search(searchTest.value) !== -1)
+      return true
+    else return false
+  }) || []
+})
+
+const paginatedData = computed(() => {
+  return searchData.value.filter((data, idx) => {
+    const startInd = ((pageNumber.value - 1) * elementPerPage.value)
+    const endInd = (pageNumber.value * elementPerPage.value) - 1
+
+    if (idx >= startInd && idx <= endInd)
+      return true
+    else return false
+  }) || []
+})
+
+function onPreviousPageClick() {
+  if (pageNumber.value > 1)
+    pageNumber.value -= 1
+}
+
+const totalPage = computed(() => {
+  const ttlContent = searchData.value.length || 0
+  const totalPage = Math.ceil(ttlContent / elementPerPage.value)
+  return totalPage
+})
+
+function onNextPageClick() {
+  if (pageNumber.value < totalPage.value)
+    pageNumber.value += 1
+}
+
+useHead({
+  title: 'The blog',
+  meta: [
+    {
+      name: 'description',
+      content: 'Here you will find all the blog posts I have written & published on this site.',
+    },
+  ],
+  titleTemplate: 'Telco - %s',
+})
+
+// Generate OG Image
+const siteData = useSiteConfig()
+defineOgImage({
+  props: {
+    title: 'The blog',
+    description: 'Here you will find all the blog posts I have written & published on this site.',
+    siteName: siteData.url,
+  },
+})
+</script>
+
 <template>
+  <main class="container mx-auto">
+    <ArchiveHero />
+
+    <div class="px-6">
+      <input
+        v-model="searchTest"
+        placeholder="Search"
+        type="text"
+        class="block"
+      >
+    </div>
+
+    <ClientOnly>
+      <div v-auto-animate>
+        <template v-for="post in paginatedData" :key="post.title">
+          <ArchiveCard
+            :path="post.path"
+            :title="post.title"
+            :date="post.date"
+            :description="post.description"
+            :image="post.image"
+            :alt="post.alt"
+            :og-image="post.ogImage"
+            :tags="post.tags"
+            :published="post.published"
+          />
+        </template>
+
+        <ArchiveCard
+          v-if="paginatedData.length <= 0"
+          title="No Post Found"
+          image="/not-found.jpg"
+        />
+      </div>
+
+      <template #fallback>
+        <!-- this will be rendered on server side -->
+        <BlogLoader />
+        <BlogLoader />
+      </template>
+    </ClientOnly>
+
+    <div class="flex justify-center items-center space-x-6 ">
+      <button :disabled="pageNumber <= 1" @click="onPreviousPageClick">
+        <Icon name="mdi:code-less-than" size="30" :class="{ 'text-sky-700 dark:text-sky-400': pageNumber > 1 }" />
+      </button>
+      <p>{{ pageNumber }} / {{ totalPage }}</p>
+      <button :disabled="pageNumber >= totalPage" @click="onNextPageClick">
+        <Icon name="mdi:code-greater-than" size="30" :class="{ 'text-sky-700 dark:text-sky-400': pageNumber < totalPage }" />
+      </button>
+    </div>
+  </main>
 </template>
