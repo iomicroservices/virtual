25f9215f2fb1620a0793aa1dd1f11208219d6e8c
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-02-28 03:20:06 +0000

    LandlineFooter

--- my-site/components/landline/footer.vue
+++ my-site/components/landline/footer.vue
@@ -1,5 +1,118 @@
 <template>
-    <div>
-        footer
-    </div>
+    <!-- =======================
+    Footer START -->
+    <footer class="pt-6">
+        <div class="container position-relative mt-sm-5">
+
+            <!-- CTA -->
+            <div class="row g-3 g-md-4 align-items-center">
+                <div class="col-md-6">
+                    <h2 class="mb-0 lh-base">We
+                        <span class="position-relative z-index-1">can't wait
+                            <!-- SVG START -->
+                            <span
+                                class="position-absolute top-50 start-50 translate-middle z-index-n1 ms-n2 d-none d-lg-block">
+                                <svg width="210" height="64" viewBox="0 0 188 53" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path class="fill-primary"
+                                        d="M3.50843 26.6791C5.35764 34.7553 19.176 38.9851 26.7629 41.4254C47.5981 47.4215 69.586 48.7695 91.2825 49.6062C107.583 50.0478 123.91 49.7573 140.135 48.1885C154.13 46.7243 169.469 45.5158 180.691 36.9051C185.618 33.0239 185.82 27.2834 181.324 22.9954C169.418 11.8166 138.589 7.57514 121.756 4.98378C103.226 2.36919 84.4557 3.171 65.8116 4.27494C77.9834 4.49573 90.1553 5.19295 102.238 6.57578C107.406 7.27301 112.802 8.35371 117.032 11.3053C113.093 10.457 109.015 10.4338 105 10.1084C76.8182 7.19166 47.1675 4.62355 19.9233 13.5713C13.9704 15.8256 2.97647 19.5442 3.50843 26.6791ZM0 25.7727C1.06393 10.1897 39.758 5.03027 53.3231 4.43763C51.1192 4.29818 48.9154 4.07739 46.7242 3.78688C50.3846 2.79915 54.121 2.04382 57.9081 1.60224C65.5583 0.939879 73.2464 0.753952 80.9092 0.323997C94.0944 -0.349987 107.305 0.0102456 120.414 1.56738C136.816 4.10063 197.384 11.3983 186.757 34.918C184.554 38.8573 180.399 41.4603 176.27 43.5403C166.163 48.6068 154.523 49.8618 143.263 51.1285C126.456 52.7902 109.559 53.3131 92.6631 52.825C67.7368 51.907 42.1772 50.501 18.6947 42.1459C11.1586 39.1943 0.126658 34.3834 0 25.7727Z" />
+                                </svg>
+                            </span>
+                            <!-- SVG END -->
+                        </span>
+                        to see what you create
+                    </h2>
+                </div>
+
+                <!-- Button -->
+                <div class="col-md-5 text-md-end ms-auto">
+                    <a class="btn btn-lg btn-primary-soft icon-link icon-link-hover mb-0" href="#custom">Personalise my number<i
+                            class="bi bi-arrow-right"></i> </a>
+                </div>
+            </div>
+
+            <!-- Divider -->
+            <hr class="my-5">
+
+            <!-- Footer Widgets -->
+            <div class="row g-4 justify-content-between">
+
+                <!-- Widget 1 START -->
+                <div class="col-lg-8 col-xl-7">
+                    <div class="row g-4">
+                        <!-- Link block -->
+                        <div class="col-6 col-md-3">
+                            <h6 class="mb-2 mb-md-4">Quick links</h6>
+                            <ul class="nav flex-column">
+                                <li class="nav-item"><a class="nav-link pt-0" href="#process">How it works</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#pricing">Pricing</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#faqs">Faqs</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#about">About us</a></li>
+                            </ul>
+                        </div>
+
+                        <!-- Link block -->
+                        <div class="col-6 col-md-3">
+                            <h6 class="mb-2 mb-md-4">Legal bits</h6>
+                            <ul class="nav flex-column">
+                                <li class="nav-item"><a class="nav-link" href="/privacy-policy.html">Privacy Policy</a></li>
+                                <li class="nav-item"><a class="nav-link" href="/terms-of-service.html">Terms of Service</a></li>
+                            </ul>
+                        </div>
+
+                        <!-- Link block -->
+                        <div class="col-md-6">
+                            <h6 class="mb-2 mb-md-4">Contact details</h6>
+                            <ul class="list-group list-group-borderless">
+                                <li class="d-flex mb-3"><i class="me-2 text-primary bi bi-headset"></i> +447999 999 553 </li>
+                                <li class="d-flex mb-3"><i class="me-2 text-primary bi bi-envelope"></i> info@createnumbers.com
+                                </li>
+                                <li class="d-flex mb-3"><i class="me-2 text-primary bi bi-clock"></i>
+                                    <p class="mb-0">Mon - Fri: <strong>09:00 - 18:00</strong> <br>Sat - Sun:
+                                        <strong>Closed</strong></p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- Widget 1 END -->
+
+                <!-- Widget 2 START -->
+                <div class="col-lg-3">
+                    <!-- Social buttons -->
+                    <h6 class="mb-2 mb-md-4">Follow on</h6>
+
+                    <ul class="list-inline mb-0 mt-3">
+                        <li class="list-inline-item"> <a class="btn btn-xs btn-icon bg-facebook" href="https://www.facebook.com/people/Create-Numbers/100067590341398/" rel="noopener noreferrer nofollow" target="_blank"><i
+                                    class="fab fa-fw fa-facebook-f lh-base"></i></a> </li>
+                        <li class="list-inline-item"> <a class="btn btn-xs btn-icon bg-instagram" href="https://www.instagram.com/createnumbers/" rel="noopener noreferrer nofollow" target="_blank"><i
+                                    class="fab fa-fw fa-instagram lh-base"></i></a> </li>
+                    </ul>
+                </div>
+                <!-- Widget 2 END -->
+            </div>
+
+            <!-- Divider -->
+            <hr class="mt-4 mb-3">
+
+            <!-- Bottom footer -->
+            <div class="d-md-flex justify-content-between align-items-center text-center text-lg-start py-3">
+                <!-- Logo -->
+                <a class="navbar-brand" href="index.html">
+                    <img class="light-mode-item navbar-brand-item h-40px" src="assets/images/logo.svg" alt="logo">
+                    <img class="dark-mode-item navbar-brand-item h-40px" src="assets/images/logo-light.svg" alt="logo">
+                </a>
+
+                <!-- Copyright link-->
+                <div class="text-body mt-3 mt-md-0">
+                    Copyrights ©2024 Create Numbers Ltd. Registered in England & Wales company number
+                    <a href="https://find-and-update.company-information.service.gov.uk/company/14217243"
+                        class="text-body text-primary-hover" rel="noopener noreferrer nofollow" target="_blank">14217243</a>
+                </div>
+                <!-- Copyright link-->
+            </div>
+        </div>
+    </footer>
+    <!-- =======================
+    Footer END -->
 </template>
