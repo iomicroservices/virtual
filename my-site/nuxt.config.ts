eecc3cede1b27c14165758ddd0f5f963dbb0208f
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-02-28 02:28:08 +0000

    added nuxt modules

--- my-site/nuxt.config.ts
+++ my-site/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/robots'
+
+  ]
 })
