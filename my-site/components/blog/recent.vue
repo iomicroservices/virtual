9ed9130c074aa9facfddf20d39df4317f5d4fcc2
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-02-29 04:03:55 +0000

    updates to feedback content

--- my-site/components/blog/recent.vue
+++ my-site/components/blog/recent.vue
@@ -61,4 +61,83 @@ useHead({
       </template>
     </div>
   </div>
+
+
+  <!-- =======================
+Blog START -->
+<Section>
+	<div class="container">
+		<!-- Title -->
+		<div class="row align-items-center g-4 mb-5">
+			<div class="col-md-6">
+				<h2 class="mb-0">Take a look at the latest articles & blogs</h2>
+			</div>
+			<div class="col-md-5 ms-auto text-sm-end">
+				<!-- Button -->
+				<a href="blog-grid.html" class="btn btn-light btn-lg mb-0">View all blogs</a>
+			</div>
+		</div>
+
+		<div class="row g-4 g-xl-5">
+			<!-- Blog item -->
+			<div class="col-md-4">
+				<article class="card card-img-scale shadow overflow-hidden h-100">
+					<!-- Image -->
+					<div class="card-img-scale-wrapper">
+						<img src="assets/images/blog/4by3/03.jpg" class="img-scale card-img-top" alt="Blog-img">
+					</div>
+
+					<div class="card-body p-4">
+						<!-- Content -->
+						<h6 class="card-title mb-4"><a href="blog-single-v1.html" class="stretched-link">Sleek and Responsive - Designing with Bootstrap and Mizzle</a></h6>
+						<ul class="nav heading-color small nav-divider mb-0">
+							<li class="nav-item">By Jacqueline Miller</li>
+							<li class="nav-item"><i class="bi bi-calendar me-1"></i>April 08, 2023</li>
+						</ul>
+					</div>
+				</article>
+			</div>
+
+			<!-- Blog item -->
+			<div class="col-md-4">
+				<article class="card card-img-scale shadow overflow-hidden h-100">
+					<!-- Image -->
+					<div class="card-img-scale-wrapper">
+						<img src="assets/images/blog/4by3/01.jpg" class="img-scale card-img-top" alt="Blog-img">
+					</div>
+
+					<div class="card-body p-4">
+						<!-- Content -->
+						<h6 class="card-title mb-4"><a href="blog-single-v1.html" class="stretched-link">Webestica Template: The Ultimate Web Design Solution</a></h6>
+						<ul class="nav heading-color small nav-divider mb-0">
+							<li class="nav-item">By Jacqueline Miller</li>
+							<li class="nav-item"><i class="bi bi-calendar me-1"></i>April 13, 2023</li>
+						</ul>
+					</div>
+				</article>
+			</div>
+
+			<!-- Blog item -->
+			<div class="col-md-4">
+				<article class="card card-img-scale shadow overflow-hidden h-100">
+					<!-- Image -->
+					<div class="card-img-scale-wrapper">
+						<img src="assets/images/blog/4by3/07.jpg" class="img-scale card-img-top" alt="Blog-img">
+					</div>
+
+					<div class="card-body p-4">
+						<!-- Content -->
+						<h6 class="card-title mb-4"><a href="blog-single-v1.html" class="stretched-link">Effortless Web Design with Mizzle - Unlock Your Creative Potential</a></h6>
+						<ul class="nav heading-color small nav-divider mb-0">
+							<li class="nav-item">By Allen Smith</li>
+							<li class="nav-item"><i class="bi bi-calendar me-1"></i>April 26, 2023</li>
+						</ul>
+					</div>
+				</article>
+			</div>
+		</div> <!-- Row END -->
+	</div>
+</Section>
+<!-- =======================
+Blog END -->
 </template>
---
+++ my-site/content/feedback/customer1.md
@@ -0,0 +1,13 @@
+---
+title: The service was fantastic
+date: 1st Mar 2023
+description: Here you will find the full text of feedback provided by customer.
+image: /06.jpg
+alt: How To Connect You Namecheap Domain With Vercel Deployed App
+ogImage: /06.jpg
+published: true
+---
+
+### {{ title }}
+
+{{ description }}
