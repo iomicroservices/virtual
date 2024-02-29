3bb49bab8f5178338485358a18235fb0bcebd84d
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-02-29 06:01:26 +0000

    remove dark mode from header

--- my-site/components/main/header.vue
+++ my-site/components/main/header.vue
@@ -45,31 +45,6 @@ function onClick(val: string) {
         
                 <!-- Buttons -->
                 <ul class="nav align-items-center dropdown-hover ms-sm-2">
-                    <!-- New dark mode -->
-                    <li class="nav-item dropdown">
-                    <ClientOnly>
-                        <button
-                        v-if="colorMode.value === 'light'"
-                        name="light-mode"
-                        title="Light"
-                        class="btn btn-link mb-0 px-2 lh-1"
-                        @click="onClick('dark')">
-                            <LogoNight />
-                        </button>
-                        <button
-                        v-if="colorMode.value === 'dark'"
-                        name="dark-mode"
-                        title="Dark"
-                        class="btn btn-link mb-0 px-2 lh-1"
-                        @click="onClick('light')">
-                            <LogoDay />
-                        </button>
-                        <template #fallback>
-                            <!-- this will be rendered on server side -->
-                            <Icon name="svg-spinners:180-ring" size="20" />
-                        </template>
-                    </ClientOnly>
-                    </li>
         
                     <!-- Buy now button -->
                     <li class="nav-item d-none d-sm-block">
