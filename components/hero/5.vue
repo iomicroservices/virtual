3f59434d10bf4d0b55a25b536601d1c96dccaacb
iomicroservices <106287942+iomicroservices@users.noreply.github.com>
2024-03-06 09:24:04 +0000

    rearrange design folder

---
+++ components/hero/5.vue
@@ -0,0 +1,46 @@
+<template>
+    
+<!-- =======================
+Main hero START -->
+<section class="pt-lg-8 pt-xl-9">
+	<div class="container position-relative pt-4 pt-lg-0">
+		<div class="row align-items-center">
+			<!-- Title -->
+			<div class="col-md-7 col-xl-6 mb-7 mb-md-0">
+				<h1 class="mb-4">Discover the solutions we offer</h1>
+				<p class="mb-4">Our experienced team is dedicated to helping you achieve your goals through innovative technology. </p>
+				<a href="contact-v1.html" class="btn btn-lg btn-dark mb-5"><i class="bi bi-telephone me-2"></i>Contact us</a>
+				<!-- Rating -->
+				<div class="d-sm-flex align-items-center">
+					<!-- Avatar group -->
+					<ul class="avatar-group mb-0 align-items-center">
+						<li class="avatar avatar-lg">
+							<img class="avatar-img rounded-circle" src="assets/images/avatar/08.jpg" alt="avatar">
+						</li>
+						<li class="avatar avatar-lg">
+							<img class="avatar-img rounded-circle" src="assets/images/avatar/12.jpg" alt="avatar">
+						</li>
+						<li class="avatar avatar-lg">
+							<div class="avatar-img rounded-circle bg-primary"><span class="text-white position-absolute top-50 start-50 translate-middle">10K+</span></div>
+						</li>
+					</ul>
+
+					<!-- Info -->
+					<div class="ms-sm-3 mt-2 mt-sm-0">
+						<p class="heading-color fw-normal mb-1">More then 10k active users! 🤩</p>
+						<a class="icon-link icon-link-hover text-primary-hover mt-auto" href="#">Join them now<i class="bi bi-arrow-right"></i> </a>
+					</div>
+				</div>
+			</div>
+
+			<!-- Image -->
+			<div class="col-md-5 ms-auto">
+				<img src="assets/images/elements/service.svg" alt="">
+			</div>
+		</div>
+	</div>	
+</section>
+<!-- =======================
+Main hero END -->
+
+</template>
--- pages/virtual-telephone/features/index.vue
+++ pages/virtual-telephone/features/index.vue
@@ -1,3 +1,53 @@
 <template>
+
+
+    <DesignServicesFeatures 
+        path="/"
+        feature1="Welcome announcement"
+        description1=""
+        button1=""
+        button1href=""
+
+        feature2="Voicemail"
+        description2=""
+        button2=""
+        button2href=""
+
+        feature3=""
+        description3=""
+        button3=""
+        button3href=""
+
+        feature4=""
+        description4=""
+        button4=""
+        button4href=""
+
+        feature5=""
+        description5=""
+        button5=""
+        button5href=""
+
+        feature6=""
+        description6=""
+        button6=""
+        button6href=""
+
+        feature7=""
+        description7=""
+        button7=""
+        button7href=""
+
+        feature8=""
+        description8=""
+        button8=""
+        button8href=""
+
+        feature9=""
+        description9=""
+        button9=""
+        button9href=""
+    />
+
     
 </template>
